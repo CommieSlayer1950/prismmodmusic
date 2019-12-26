f8b42a984444a78742a25468df11dfdc10c639b2
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-12-26 01:19:51 -0600

    Update PrismachinePianoTile.cs

--- Tiles/Pianos/PrismachinePianoTile.cs
+++ Tiles/Pianos/PrismachinePianoTile.cs
@@ -16,20 +16,15 @@ namespace prismmodmusic.Tiles.Pianos
             Main.tileNoAttach[Type] = true;
             Main.tileTable[Type] = false;
             Main.tileLavaDeath[Type] = true;
-            TileObjectData.newTile.CopyFrom(TileObjectData.Style6x3);
+            TileObjectData.newTile.CopyFrom(TileObjectData.Style3x2);
             TileObjectData.newTile.Height = 2;
             TileObjectData.newTile.Width = 3;
             TileObjectData.newTile.CoordinateHeights = new[] { 16, 16 };
-            TileObjectData.newTile.Origin = new Point16(3, 1);
             TileObjectData.addTile(Type);
             AddToArray(ref TileID.Sets.RoomNeeds.CountsAsTable);
             ModTranslation name = CreateMapEntryName();
             name.SetDefault("PrismachinePiano");
             AddMapEntry(new Color(200, 200, 200), name);
-            disableSmartCursor = true;
-            adjTiles = new int[] { TileID.WorkBenches };
-            Main.tileFrameImportant[Type] = true;
-            Main.tileLighted[Type] = true;
         }
 
         public override void KillMultiTile(int i, int j, int frameX, int frameY)
@@ -37,9 +32,11 @@ namespace prismmodmusic.Tiles.Pianos
             Item.NewItem(i * 16, j * 16, 32, 16, mod.ItemType("PrismachinePiano"));
         }
 
-        public override void RightClick(int i, int j)
+        public override bool NewRightClick(int i, int j)
         {
-            
+            Main.PlaySound(mod.GetSoundSlot(SoundType.Music, "Sounds/Music/PrismachineTheme"));
+            return true;
         }
+
     }
 }
