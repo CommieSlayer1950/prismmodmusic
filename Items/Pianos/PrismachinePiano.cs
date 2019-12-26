8af5a5621296ebe8145e848bd60e7c9b9e2675a3
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-12-26 00:36:20 -0600

    Update

---
+++ Items/Pianos/PrismachinePiano.cs
@@ -0,0 +1,18 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace prismmodmusic.Items.Pianos
+{
+    class PrismachinePiano : ModItem
+    {
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Prismachine Piano");
+        }
+
+    }
+
+
+}
