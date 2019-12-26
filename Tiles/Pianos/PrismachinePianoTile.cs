eeab73e16eb7601c512a8ff057b9e4cd7cadc861
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-12-26 00:57:16 -0600

    Update PrismachinePianoTile.cs

--- Tiles/Pianos/PrismachinePianoTile.cs
+++ Tiles/Pianos/PrismachinePianoTile.cs
@@ -5,7 +5,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.ObjectData;
 
-namespace prismmod.Tiles
+namespace prismmodmusic.Tiles
 {
     public class PrismachinePianoTile : ModTile
     {
