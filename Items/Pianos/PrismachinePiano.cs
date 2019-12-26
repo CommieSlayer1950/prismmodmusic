1f9bba2bb2bdb5e8b6706a303d3a25036cff86e9
CommieSlayer1950 <nathan.leakeas@gmail.com>
2019-12-26 00:56:11 -0600

    STUFF

--- Items/Pianos/PrismachinePiano.cs
+++ Items/Pianos/PrismachinePiano.cs
@@ -1,6 +1,7 @@
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using prismmodmusic;
 
 namespace prismmodmusic.Items.Pianos
 {
@@ -10,6 +11,29 @@ namespace prismmodmusic.Items.Pianos
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Prismachine Piano");
+            Tooltip.SetDefault("Plays a piano version of the Prismachine Theme");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 28;
+            item.height = 28;
+            item.maxStack = 1;
+            item.useStyle = 1;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.createTile = ModContent.TileType<>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.AddIngredient(TileID.Pianos, 1);
+            Mod prismmod = ModLoader.GetMod("prismmod");
+            recipe.AddIngredient(prismmod.GetItem("PrismaticChunk"));
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
 
     }
